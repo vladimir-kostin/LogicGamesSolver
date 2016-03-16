0173a85ef385c8a2dd925a9822cb38d91de0ec86
Vladimir Kostin <vladimir.s.kostin@gmail.com>
2016-03-16 18:47:09 +0200

    in progress

--- src/main/com/vkostin/Puzzle2.java
+++ src/main/com/vkostin/Puzzle2.java
@@ -35,7 +35,58 @@ public class Puzzle2 implements IPuzzle {
   }
 
   private boolean doesTaskCellHaveAnyErrors(TaskCell taskCell) {
+    if(0 < taskCell.getSumOfValuesBelow()) {
+      List<ValueCell> valueCellsBelow = getValueCellsBelow(taskCell);
+      if(containsError(valueCellsBelow, taskCell.getSumOfValuesBelow())) { return true; }
+    }
+    if(0 < taskCell.getSumOfValueOnTheRight()) {
+      List<ValueCell> valueCellsOnTheRight = getValueCellsOnTheRight(taskCell);
+      if(containsError(valueCellsOnTheRight, taskCell.getSumOfValueOnTheRight())) { return true; }
+    }
     return false;
+  }
+
+  private static boolean containsError(List<ValueCell> valueCells, int expectedSumOfValues) {
+    int actualSumOfValues = valueCells.stream()
+            .filter(ValueCell::hasProperValue)
+            .mapToInt(ValueCell::getValue)
+            .sum();
+
+    if(expectedSumOfValues < actualSumOfValues) { return true; }
+
+    boolean containsUnresolvedValueCells = valueCells.stream()
+            .anyMatch(ValueCell::isUnsolved);
+
+    if(!containsUnresolvedValueCells && expectedSumOfValues != actualSumOfValues) { return  true; }
+
+    long properValuesCount = valueCells.stream()
+            .filter(ValueCell::hasProperValue)
+            .count();
+
+    long distinctProperValuesCount = valueCells.stream()
+            .filter(ValueCell::hasProperValue)
+            .mapToInt(ValueCell::getValue)
+            .distinct()
+            .count();
+
+    return (properValuesCount != distinctProperValuesCount);
+  }
+
+  private List<ValueCell> getValueCellsOnTheRight(TaskCell taskCell) {
+    for (List<Cell> row : cells) {
+      int ndx = indexOfCellInRow(taskCell, row);
+      if (-1 == ndx) continue;
+    }
+  }
+
+  private int indexOfCellInRow(Cell cell, List<Cell> row) {
+    for (Cell c : row) {
+      if (c == cell) { return row.indexOf(c); }
+    }
+    return -1;
+  }
+
+  private List<ValueCell> getValueCellsBelow(TaskCell taskCell) {
   }
 
 }

b47141228748cac3f8b235f6c2c2f203ae10fb85
Vladimir Kostin <vladimir.s.kostin@gmail.com>
2016-06-10 12:58:38 +0300

    tenner-grid: unneeded stuff removed

--- src/test/com/vkostin/puzzle/tennerGrid/SolversPerformanceTest.java
+++ src/test/com/vkostin/puzzle/tennerGrid/SolversPerformanceTest.java
@@ -11,34 +11,11 @@ public class SolversPerformanceTest {
 
   private Parser parser = new Parser(PuzzleAsArray::new);
 
-  private BetterSolver betterSolver = new BetterSolver();
   private AnotherSolver anotherSolver = new AnotherSolver();
-
-  @Test
-  public void testBetterSolver() {
-    TestUtils.assertSolving(TestData.ALL_TEST_DATA, parser, betterSolver);
-  }
 
   @Test
   public void testAnotherSolver() {
     TestUtils.assertSolving(TestData.ALL_TEST_DATA, parser, anotherSolver);
-  }
-
-  @Test
-  public void testAnotherSolver10times() throws Exception {
-
-    for (int i = 0; i < 10; i++) {
-      testAnotherSolver();
-    }
-
-  }
-
-  @Test
-  public void testBetterSolver10times() throws Exception {
-    for (int i = 0; i < 10; i++) {
-      testBetterSolver();
-    }
-
   }
 
 }
